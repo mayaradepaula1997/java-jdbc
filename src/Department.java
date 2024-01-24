8454085730140221115b99347799eeb7e4cd7721
mayaradepaula1997 <mayara.paula1997@gmail.com>
2024-01-24 21:19:01 -0200

    Classe Departamento

---
+++ src/Department.java
@@ -0,0 +1,52 @@
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable { // para que nosso objetos possam ser transformados em sequencia de bits
+
+    private Integer id;
+    private String name;
+
+    public Department (){
+
+    }
+    public Department ( Integer id, String name){
+        this.id = id;
+        this.name =  name;
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override //seu comparado pelo conteudo, e não pela referencia de ponteiros
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Department" +
+                "id=" + id +
+                ", name=" + name;
+    }
+}
---
+++ src/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+
+    public static void main(String[] args) {
+
+        Department obj = new Department(1,"Maria");
+        System.out.println(obj);
+    }
+}
