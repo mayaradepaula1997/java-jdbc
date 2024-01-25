b0b3433d01e4d8ccbf6f1af7a2fa21a3db95d40e
mayaradepaula1997 <mayara.paula1997@gmail.com>
2024-01-24 22:31:41 -0200

    Interface DepartmentDao e SellerDao

---
+++ src/DepartmenDao.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public interface DepartmenDao {
+
+    void insert (Department obj); // responsavel por inserir no banco de dados esse obj
+    void updat (Department obj);
+    void deleteById (Integer id);
+    Department findById (Integer id);
+    //responsavel por pegar o Id e consultar no bando de dados o obj com esse Id, se existir
+    // vai retorna, senão vai retorna null
+    List <Department> findAll();
+    //responsaval por retorna todos os departamentos
+}
