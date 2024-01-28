86083dd13f675a9dc2a79b070ae441808b209a8b
mayaradepaula1997 <mayara.paula1997@gmail.com>
2024-01-28 15:08:35 -0200

    implementar findAll, buscar todos os vendedores do departamento

--- src/Program.java
+++ src/Program.java
@@ -16,13 +16,20 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.criarSellerDao(); //injeção de dependencia
 
-        System.out.println("=== TEST 1:  seller findById =====");
+        System.out.println("\n=== TEST 1:  seller findById =====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
-        System.out.println("=== TEST 2:  seller findByDepartment =====");
+        System.out.println("\n=== TEST 2:  seller findByDepartment =====");
         Department department = new Department( 2, null);
         List<Seller> list = sellerDao.findByDepartament(department);
+        for (Seller obj : list){
+            System.out.println(obj);
+
+        }
+
+        System.out.println("\n=== TEST 3:  seller findAll =====");
+        list = sellerDao.findAll();
         for (Seller obj : list){
             System.out.println(obj);
         }
